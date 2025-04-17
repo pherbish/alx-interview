78331ff433e1410a1f84c7e24edcfc35640a4463
Pherbish Orwa <ochiengpherbish2018@gmail.com>
2025-04-17 11:06:04 +0300

    Pascal Triangle

---
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -0,0 +1,19 @@
+def pascal_triangle(n):
+    """
+    Returns a list of lists of integers representing Pascal’s triangle of n
+    """
+    if n <= 0:
+        return []
+
+    triangle = [[1]]  
+
+    for i in range(1, n):
+        prev_row = triangle[-1]
+        
+        row = [1]  
+        for j in range(1, i):
+            row.append(prev_row[j - 1] + prev_row[j])
+        row.append(1)  
+        triangle.append(row)
+
+    return triangle
